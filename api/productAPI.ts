8e46cdf6812947ac23ed5b1c4e6f29684b426884
Nikita <nikita.tikhonov@viaccess-orca.com>
2023-09-14 19:25:52 +0300

    update products request

--- api/productAPI.ts
+++ api/productAPI.ts
@@ -3,8 +3,12 @@ import {Product} from '../state/types';
 const PRODUCTS_URI =
   'https://dev-autibfbfinllwfr.api.raw-labs.com/your/endpoint/path';
 
+type ProductResponse = {
+  products: Product[];
+};
+
 export const fetchProducts = async (): Promise<Product[]> => {
   const response = await fetch(PRODUCTS_URI);
-  const products: Product[] = await response.json();
-  return products;
+  const productResponse: ProductResponse = await response.json();
+  return productResponse.products;
 };
